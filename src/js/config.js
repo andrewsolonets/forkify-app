d03c3e0890d492579b9f719f204edf55f5280d12
Andrew Solonets <andrewsolonets@gmail.com>
2022-06-23 12:06:02 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e6ff77be-8fd9-44bc-aa72-45f79fd33731';
+export const MODAL_CLOSE_SEC = 2.5;
